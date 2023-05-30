4aa3fc65b6401890f8607404f104315b60dec443
Krazza <muondove@gmail.com>
2023-05-30 20:52:05 +0300

    anecdotes with votes done

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -11,19 +11,56 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
   ]
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0);
+  const [anecdoteOfTheDay, setAnecdoteOfTheDay] = useState("");
+  const [votes, setVotes] = useState(new Uint16Array(anecdotes.length));
+
+  const Next = () =>{
+	GenerateAnecdote()
+  }
 
   const GenerateAnecdote = () =>{
 	const randomIndex = Math.floor(Math.random() * anecdotes.length)
 	setSelected(randomIndex);
   }
 
+  const Vote = () =>{
+	const newVotes = votes;
+	newVotes[selected]+=1;
+	setVotes([...newVotes]);
+	setAnecdoteOfTheDay(anecdotes[GetAnecdoteOfTheDayIndex()]);
+  }
+
+  const GetAnecdoteOfTheDayIndex = () =>{
+	let max = 0;
+	let maxIndex = 0;
+	for(let i = 0; i < votes.length; i++){
+		if(max < votes[i])
+		{
+			max = votes[i];
+			maxIndex = i;
+		}
+	}
+	return maxIndex;
+  }
+
   return (
     <div>
-    	{anecdotes[selected]}
 		<div>
-			<Button text={"Next"} handleClick={()=> GenerateAnecdote()}/>
+			<h1>{"Random anecdote"}</h1>
+    		<p>{anecdotes[selected]}</p>
+			<p>{`${votes[selected]} votes.`}</p>
+			<Button text={"Next"} handleClick={()=> Next()}/>
+			<Button text={"Vote"} handleClick={()=> Vote()}/>
 		</div>
+		{anecdoteOfTheDay === "" ? 
+			<div><p>{"Vote for an anecdote to reveal the anecdote of the day."}</p> </div> 
+			: 
+			<div>
+			<h2>{"Anecdote of the day"}</h2>
+			<p>{anecdoteOfTheDay}</p>
+			<p>{`Anecdote of the day has ${votes[GetAnecdoteOfTheDayIndex()]} votes.`}</p>
+		</div>}
     </div>
   )
 }

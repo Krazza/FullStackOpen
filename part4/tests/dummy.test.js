d24382e095944c4012bf076fbc762df20f83bc3a
Krazza <muondove@gmail.com>
2023-06-13 19:11:48 +0300

    4.5 done

--- part4/tests/dummy.test.js
+++ part4/tests/dummy.test.js
@@ -6,3 +6,51 @@ test("dummy returns one", () => {
     const result = listHelper.dummy(blogs);
     expect(result).toBe(1);
 })
+
+describe("total likes", () => {
+    const listWithOneBlog = [
+        {
+            title: "Valorant guides",
+            author: "SEN TENZ",
+            url: "https://sentenzguides.com",
+            likes: 5000,
+            id: "648877f99e92a32a46c0fb27"
+        }
+    ]
+
+    test("when list has only one blog, equals the likes of that", () => {
+        const result = listHelper.totalLikes(listWithOneBlog);
+        expect(result).toBe(5000);
+    })
+})
+
+describe("favourite blog", () => {
+    const blogList = [
+        {
+            title: "Clever books",
+            author: "Stephen King",
+            url: "https://king.com",
+            likes: 142,
+            id: "648877f91e22a33a46c0fb27"
+        },
+        {
+            title: "Tasty fruits",
+            author: "Kevin",
+            url: "https://kevinsfruits.com",
+            likes: 21,
+            id: "148877f99e92a32a46c0fb23"
+        },
+        {
+            title: "Valorant guides",
+            author: "SEN TENZ",
+            url: "https://sentenzguides.com",
+            likes: 5000,
+            id: "648877f99e92a32a46c0fb27"
+        }
+    ]
+
+    test("find the blog with most likes and compare", () => {
+        const favBlog = listHelper.favouriteBlog(blogList);
+        expect(favBlog).toEqual(blogList[2]);
+    })
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,26 @@ const dummy = (blogs) => {
     return 1;
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        return sum + item.likes
+    }
+    return blogs.reduce(reducer, 0);
+}
+
+const favouriteBlog = (blogs) => {
+    let favouriteBlog = blogs[0];
+
+    blogs.forEach(blog => {
+        if(favouriteBlog.likes < blog.likes)
+            favouriteBlog = blog;
+    })
+
+    return favouriteBlog;
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favouriteBlog
 }

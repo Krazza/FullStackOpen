a78b643a25221334dcffa4039bd476260a4e51b8
Krazza <muondove@gmail.com>
2023-06-22 16:41:07 +0300

    5.7 done

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,44 @@
-const Blog = ({blog}) => (
-  <div>
-    {`"${blog.title}" by ${blog.author}`}
-  </div>  
-)
+import React, { useState } from "react";
+
+const Blog = ({blog}) => {
+
+  const [showDetails, setShowDetails] = useState(false);
+
+  const handleToggle = (event) => {
+    event.preventDefault();
+    setShowDetails(!showDetails);
+  }
+
+  const inlineStyle = {
+    display : "inline"
+  }
+
+  const blogStyle = {
+    marginBottom : "20px",
+    borderStyle : "solid",
+    width : "500px",
+    padding : "5px"
+  }
+
+  const buttonStyle = {
+    display : "inline",
+    margin : "8px"
+  }
+
+  return(
+  <div style={blogStyle}>
+    {showDetails ? 
+    <div >
+      <h4 style={inlineStyle}>{blog.title}</h4>{showDetails && <button style={buttonStyle} onClick={handleToggle}>{"hide"}</button>}
+      <p>{"AUTHOR : "}{blog.author}</p>
+      <p>{"URL : "}{blog.url}</p>
+      <p style={inlineStyle}>{"LIKES : "}{blog.likes}</p><button style={buttonStyle}>{"like"}</button>
+    </div>
+    :
+    <div>
+      <h4 style={inlineStyle}>{blog.title}</h4>{!showDetails && <button style={buttonStyle} onClick={handleToggle}>{"view"}</button>}
+    </div>}
+  </div>)  
+}
 
 export default Blog

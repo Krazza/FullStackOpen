44ef3708cd886135bffebd95ceb6afc2b262fb4e
Krazza <muondove@gmail.com>
2023-07-20 13:37:47 +0300

    6.16 done

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -4,8 +4,6 @@ import { Provider } from 'react-redux'
 import store from './store'
 import App from './App'
 
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<Provider store={store}>
 		<App />
